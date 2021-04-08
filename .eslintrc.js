ada2fd01b85f6986f15924da56dd616d9995f5d4
= <=>
2021-04-08 16:13:06 +0200

    scheiss linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,5 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript/interface-name-prefix": "always",
   }
 };
--- src/modules/interfaces.ts
+++ src/modules/interfaces.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export interface IArmour {
   name: string;
   defense: number;
